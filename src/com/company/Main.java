917c9d3e6e852221f77b8700097c71d134cd9e41
davitjikia <davitjiko@gmail.com>
2021-03-06 11:45:51 -0500

    first comiit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("hello batch 8");
+            Thread.sleep(2000);
+        }
+
+    }
+}
