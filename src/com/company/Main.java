404f09851884db6a31928feca4f4e7f94c91bfe8
davitjikia <davitjiko@gmail.com>
2021-03-07 10:55:19 -0500

    another commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 0; i < 5; i++){
-            Sysm.out.println("hello batch 8");
+            ystem.out.println("hello batch 8");
             Thread.sleep(2000);
         }
 
